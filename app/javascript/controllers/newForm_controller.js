4dae7796c7e146ea48e777d3ce0cb2f6d81f0b13
Stephane Baroux <stephane.baroux@gmail.com>
2021-11-24 15:42:40 +0100

    list of appropriate services shows on focus of the input field, and disappears on blur

--- app/javascript/controllers/newForm_controller.js
+++ app/javascript/controllers/newForm_controller.js
@@ -3,19 +3,30 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["service", "category", "autocomplete"]
   connect() {
-    this.serviceTarget.insertAdjacentHTML('afterend', `<div id="autocomplete-box" data-newForm-target='autocomplete'></div>`)
+    this.serviceTarget.insertAdjacentHTML('afterend', `<div id="autocomplete-services" data-newForm-target='autocomplete'></div>`)
   }
 
-  selectCategory(e) {
-    console.log(this.autocompleteTarget)
-    const query = encodeURIComponent(this.categoryTarget.value)
-    const url = `http://localhost:3000/search/${query}`;
-    fetch(url)
-      .then(res => res.text())
-      .then(data => {
-        console.log(data)
-        this.autocompleteTarget.innerHTML = data;
-      })
+  showServices(e) {
+    console.log('focused')
+    const category = this.categoryTarget.value
+    if (category) {
+      const query = encodeURIComponent(category)
+      const url = `http://localhost:3000/search/${query}`;
+      fetch(url)
+        .then(res => res.text())
+        .then(data => {
+          this.autocompleteTarget.classList.remove('hidden');
+          this.autocompleteTarget.innerHTML = data;
+        })
+    }
+  }
+
+  hideServices() {
+    this.autocompleteTarget.classList.add('hidden');
+  }
+
+  selectService(event){
+    this.serviceTarget.value = event.currentTarget.children[0].innerText
   }
 
 }
