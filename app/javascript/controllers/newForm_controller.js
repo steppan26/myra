9b5fb31a663b7f3d13771120ed298d730a2500b5
Stephane Baroux <stephane.baroux@gmail.com>
2021-11-24 18:49:28 +0100

    selecting a service displays its offers. Selecting an offer pre-populates the relevant input fiels whilst keeping them hidden, if the user selects a custom offer the input fields are displayed to the user

--- app/javascript/controllers/newForm_controller.js
+++ app/javascript/controllers/newForm_controller.js
@@ -1,13 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["serviceInput", "categoryInput", "autocompleteServices", "serviceCustom", "servicePreset", "service"]
+  static targets = ["serviceInput", "categoryInput", "autocompleteServices", "autocompleteOffers", "serviceCustom",
+                    "servicePreset", "service", "offer", "offerCustom", "offerPreset", "priceInput", "frequencyInput"]
   connect() {
     this.categoryInputTarget.insertAdjacentHTML('afterend', `
-        <div id="autocomplete-services" data-newForm-target='autocompleteServices'></div>
-        <div id="autocomplete-offers" data-newForm-target='autocompleteOffers'></div>
+        <div class="autocomplete-wrapper" id="autocomplete-services" data-newForm-target='autocompleteServices'></div>
+        <div class="autocomplete-wrapper" id="autocomplete-offers" data-newForm-target='autocompleteOffers'></div>
         `);
-  }
+  };
 
   showServices() {
     this.autocompleteServicesTarget.classList.remove('hidden');
@@ -21,25 +22,26 @@ export default class extends Controller {
           this.autocompleteServicesTarget.innerHTML = data;
         })
     }
-  }
+  };
+
+  _hideServices() {
+    this.autocompleteServicesTarget.classList.add('hidden');
+  };
 
   showOffers(){
-    const autocompleteOffers = document.querySelector('#autocomplete-offers');
-    console.log(this.serviceInputTarget.value);
+    this.autocompleteOffersTarget.classList.remove('hidden');
     const query = encodeURIComponent(this.serviceInputTarget.value);
     const url = `http://localhost:3000/searchOffer/${query}`;
     fetch(url)
       .then(res => res.text())
       .then(data => {
-        autocompleteOffers.innerHTML = data;
+        this.autocompleteOffersTarget.innerHTML = data;
       });
-  }
+  };
 
-  hideServices() {
-    setInterval(() => {
-      this.autocompleteServicesTarget.classList.add('hidden');
-    }, 2000);
-  }
+  _hideOffers() {
+    this.autocompleteOffersTarget.classList.add('hidden');
+  };
 
   selectService(event){
     const services = this.serviceTargets;
@@ -50,15 +52,42 @@ export default class extends Controller {
         service.classList.remove('active');
       }
     });
-    console.log(this.serviceInputTarget.parentElement);
     if (event.currentTarget === this.serviceCustomTarget){
       this.serviceInputTarget.value = "";
       this.serviceInputTarget.parentElement.classList.remove('hidden');
+      this._hideOffers();
     } else {
       this.serviceInputTarget.parentElement.classList.add('hidden');
       this.serviceInputTarget.value = event.currentTarget.children[0].innerText;
       this.showOffers();
     }
-  }
+  };
 
+  selectOffer(event) {
+    const offers = this.offerTargets;
+    offers.forEach(offer => {
+      if (offer === event.currentTarget) {
+        offer.classList.add('active');
+      } else {
+        offer.classList.remove('active');
+      }
+    });
+    const price = this.priceInputTarget;
+    const frequency = this.frequencyInputTarget;
+    console.log(event.currentTarget);
+    if (event.currentTarget === this.offerCustomTarget) {
+      price.parentElement.classList.remove('hidden');
+      frequency.parentElement.classList.remove('hidden');
+      price.value = "";
+      frequency.value = "";
+
+      console.log(price, frequency);
+    } else {
+      price.parentElement.classList.add('hidden');
+      price.value = event.currentTarget.dataset.offerPrice
+      frequency.parentElement.classList.add('hidden');
+      frequency.value = event.currentTarget.dataset.offerFrequency
+      console.log(price, frequency);
+    };
+  };
 }
