7feb3373567cd9af6e84deefb4c1da70a36f06c4
gauthier chabanne <gauthierchabanne@hotmail.fr>
2021-11-29 17:10:35 +0100

    On the way

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_29_091610) do
+ActiveRecord::Schema.define(version: 2021_11_29_160249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 2021_11_29_091610) do
     t.string "provider"
     t.string "uid"
     t.bigint "budget_id"
+    t.integer "global_budget", default: 0
     t.index ["budget_id"], name: "index_users_on_budget_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
