fe890d81cef31b2c790a737bc6e43363a12daf5b
gauthier chabanne <gauthierchabanne@hotmail.fr>
2021-11-30 11:42:00 +0100

    What a big load of work

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   get "/dashboard", to: "pages#dashboard"
   get "settings", to: "pages#settings"
+  get "infos", to: "pages#infos"
 
   # 'new' form routes
   get "/searchService/:query", to: 'subscriptions#display_services'
