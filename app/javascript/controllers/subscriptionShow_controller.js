33cc66004048c71a534484951c6a2508fab7e254
Stephane Baroux <stephane.baroux@gmail.com>
2021-11-30 14:40:29 +0100

    show and hide the edit states for subscriptions

---
+++ app/javascript/controllers/subscriptionShow_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["show", "edit", "renewalInput", "renewalElement", "addInfoInput", "addInfoElement", "delayInput", "delayElement"]
+
+
+  connect(){
+  }
+
+  display_edit(){
+    this.showTargets.forEach((element) => {
+      element.classList.add('hidden');
+    })
+    this.editTargets.forEach((element) => {
+      element.classList.remove('hidden');
+    })
+  }
+
+  display_show(){
+    this.showTargets.forEach((element) => {
+      element.classList.remove('hidden');
+    })
+    this.editTargets.forEach((element) => {
+      element.classList.add('hidden');
+    })
+  }
+
+  save_changes(){
+    console.log('saving changes');
+  }
+
+  discard_changes(){
+    this.renewalInputTarget.value = this.renewalElementTarget.innerText;
+    this.addInfoInputTarget.value = this.addInfoElementTarget.innerText;
+    this.delayInputTarget.value = this.delayElementTarget.innerText;
+    this.display_show();
+  }
+}
