42d18a248347b941dc6b96b07b30efde592a9d6b
gauthier chabanne <gauthierchabanne@hotmail.fr>
2021-11-30 12:03:41 +0100

    Better

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -2,4 +2,5 @@ class Budget < ApplicationRecord
   belongs_to :user
   has_many :budget_items, dependent: :destroy
   has_many :subscriptions, through: :budget_items
+  monetize :price_per_month_cents
 end
